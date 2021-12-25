9d2cacaa3c519c81c11a2f0b0ad1f380b739867f
Almog <shoralmog@gmail.com>
2021-12-25 19:30:53 +0200

    init node

--- node.h
+++ node.h
@@ -1,8 +1,10 @@
-//
-// Created by shora on 25/12/2021.
-//
-
 #ifndef C_EX4_NODE_H
 #define C_EX4_NODE_H
+
+struct node{
+    int id;
+    int edgesIn[];
+    int edgesOut[];
+};
 
 #endif //C_EX4_NODE_H
