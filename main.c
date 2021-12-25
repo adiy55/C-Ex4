27f4e96fc661af5942a5eb9bfb4a68bdbc8ea252
Almog <shoralmog@gmail.com>
2021-12-25 19:43:21 +0200

    base main

--- main.c
+++ main.c
@@ -4,5 +4,30 @@
 #include "algo.h"
 
 int main() {
+    int run = 1, len_path;
+    char action = 'A';
+    while (run) {
+        // action A: insert a new graph
+        // action B: create new node with new out edges
+        // action D: delete node
+        // action S: shortest path between two given nodes
+        // action T: TSP
+        scanf("%c", &action);
+      if (action == 'A') {
+            insert(matrix);
+        } else {
+            calc_matrix(); // calculate the shortest path
+            if (action == 'B') {
+                is_there_a_route(shortest_path) ? printf("True\n") : printf("False\n");
+            } else if (action == 'D') {
+                len_path = what_is_the_shortest_route(shortest_path);
+                len_path ? printf("%d\n", len_path) : printf("-1\n");
+            } else if (action == 'S'){
+
+            } else if (action == 'T'){
+
+            }
+        }
+    }
     return 0;
 }
