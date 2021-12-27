4ee6fdcc4b5f47cf8b56b4630445aa6d8a28a91e
adiy55 <adiyafe55@gmail.com>
2021-12-27 13:25:03 +0200

    Update node.c

--- node.c
+++ node.c
@@ -1,27 +1,27 @@
-#include <stdio.h>
-#include "node.h"
-#include "edge.h"
-
-int nodeInit(int id) {
-
-}
-
-int addEdgeIn() {
-
-}
-
-int addEdgeOut() {
-
-}
-
-int deleteNode(int){
-
-}
-
-int deleteEdgesIn(int){
-
-}
-
-int deleteEdgesOut(int){
-
-}
+//#include <stdio.h>
+//#include "node.h"
+//#include "edge.h"
+//
+//int nodeInit(int id) {
+//
+//}
+//
+//int addEdgeIn() {
+//
+//}
+//
+//int addEdgeOut() {
+//
+//}
+//
+//int deleteNode(int){
+//
+//}
+//
+//int deleteEdgesIn(int){
+//
+//}
+//
+//int deleteEdgesOut(int){
+//
+//}
